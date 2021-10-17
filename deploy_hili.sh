1036f7ac7173debb866630f634ad0cd1b32febd1
XiaoInk <xiaoqiangink@outlook.com>
2021-10-17 10:23:26 +0800

    update deploy

--- deploy_hili.sh
+++ deploy_hili.sh
@@ -18,6 +18,6 @@ git add -A
 git commit -m 'deploy'
 
 
-git push -f https://github.com/wearebug/wearebug.github.io.git master
+git push -f git@github.com:wearebug/wearebug.github.io.git master
 
 cd -
