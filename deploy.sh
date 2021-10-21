bebd5c83b230ec890a023fabfab5119e4cba5044
XiaoInk <xiaoqiangink@outlook.com>
2021-10-21 12:36:18 +0800

    更新语言包

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,17 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git remote set-url origin https://github.com/wearebug/pisaai.com.git
 # 生成静态文件
 yarn build
 
 # 进入生成的文件夹
 cd dist
+
+# 生成域名验证文件
+echo dev.pisaai.com > CNAME
+
+# 修复Github pages 部署时, history 模式路由访问子路由提示 404 的问题
+cat index.html > 404.html
 
 # git init
 git init
@@ -16,6 +21,6 @@ git add -A
 git commit -m 'deploy'
 
 
-git push -f https://github.com/wearebug/pisaai.com.git master:gh-pages
+git push -f git@github.com:wearebug/wearebug.github.io.git master
 
 cd -
