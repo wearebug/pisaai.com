4d0bbea75c9a0d6b5365e4f4dc1fbcff25a5d51e
Mark HOU <wearebug@gmail.com>
2021-10-28 20:24:21 +0800

    名称从lang.js读取

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color="black" dark>
       <v-container class="d-flex justify-space-between pa-0">
         <div class="d-flex align-center pa-0">
-          <v-img alt="高光照片优化" class="shrink" contain src="../assets/logo.png" transition="scale-transition" width="120" eager />
+          <v-img alt="{{ $vuetify.lang.t('$vuetify.name') }}" class="shrink" contain src="../assets/logo.png" transition="scale-transition" width="120" eager />
           <!-- <h1 class="ml-4 text-h6">{{ $vuetify.lang.t('$vuetify.name') }}</h1> -->
         </div>
         <v-spacer></v-spacer>
